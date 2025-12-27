d424d389584122724d52c7ab9042a69cdbb8be24
aqilahJ925 <nuraqj925@gmail.com>
2025-12-27 16:10:41 +0800

    farah part

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,6 @@ class _MyAppState extends ConsumerState<MyApp> {
     ref.read(userProvider.notifier).update((state) => userModel);
   }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ref
